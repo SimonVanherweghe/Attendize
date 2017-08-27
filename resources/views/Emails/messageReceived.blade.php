e3c16304202a75eb231272c4fb9e08a3baf303c5
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2017-08-27 12:15:39 +0200

    Escaping issue

--- resources/views/Emails/messageReceived.blade.php
+++ resources/views/Emails/messageReceived.blade.php
@@ -3,7 +3,7 @@
 @section('message_content')
 
     <p>{{trans('emails.hello')}}</p>
-    <p>{{trans('emails.message.intro',['name'=>(isset($sender_name) ? $sender_name : $event->organiser->name) , 'event'=> $event->title])}}</p>
+    <p>{!! trans('emails.message.intro',['name'=>(isset($sender_name) ? $sender_name : $event->organiser->name) , 'event'=> $event->title]) !!}</p>
     <p style="padding: 10px; margin:10px; border: 1px solid #f3f3f3;">
         {{nl2br($message_content)}}
     </p>
